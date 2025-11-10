28a1bf16a764eb1113a1d4275fd4167548cebe95
Benjamin <bennguyen8180@gmail.com>
2025-11-11 00:26:12 +1100

    build script and gitignore

---
+++ asciiart/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+mkdir -p dist
+
+for os in linux windows darwin; do
+  for arch in amd64 arm64; do
+    outname="../dist/asciiart-${os}-${arch}"
+    [ "$os" = "windows" ] && outname+=".exe"
+    echo "Building $outname..."
+    CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags="-s -w" -o $outname ./cmd/main/.
+  done
+done
