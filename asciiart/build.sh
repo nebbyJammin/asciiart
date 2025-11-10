181b53a200e8146c3a27d370c0693a3350820d6f
Benjamin <bennguyen8180@gmail.com>
2025-11-11 00:31:28 +1100

    fixed typo

--- asciiart/build.sh
+++ asciiart/build.sh
@@ -8,6 +8,6 @@ for os in linux windows darwin; do
     outname="../dist/asciiart-${os}-${arch}"
     [ "$os" = "windows" ] && outname+=".exe"
     echo "Building $outname..."
-    CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags="-s -w" -o $outname ./cmd/main/.
+    CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags="-s -w" -o $outname ./cmd/asciiart/.
   done
 done
